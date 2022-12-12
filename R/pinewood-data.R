cdc09ebd377e8882c22a13a670318ca6852d7fe4
Fiona Seaton <fseaton@ceh.ac.uk>
2022-12-12 10:04:48 +0000

    Switching to trees for pinewood data

--- R/pinewood-data.R
+++ R/pinewood-data.R
@@ -1,17 +1,16 @@
 #' Vegetation data from native pinewoods in Scotland, 1971
 #'
-#' This is the plant data from a series of plots across native Scots Pine woodlands
-#' in Scotland surveyed in 1971. It contains 34 plant species measured over 25 sites.
+#' This is the tree data from a series of plots across native Scots Pine woodlands
+#' in Scotland surveyed in 1971. It contains 16 tree species measured over 265 plots.
 #' For further information on this data see Wood and Bunce (2016).
 #'
 #' This data is made available under the terms of the Open Government Licence.
 #'
 #' @format
-#' A data frame with 25 rows and 34 columns. Each column is a
-#'   species, or species aggregates and each row is a site. The site identity is marked
+#' A data frame with 265 rows and 16 columns. Each column is a
+#'   species, or species aggregates and each row is a plot. The plot identity is marked
 #'   in the rownames of the data frame. Every entry is given as the number of
-#'   plots that species is observed in within the site (each site has 16 plots, other
-#'   than a small number of exceptions, detailed in pinewood_env.
+#'   individuals of that species that is observed in within the plot.
 #'
 #' @references Wood, C. M., & Bunce, R. G. H. (2016). Ecological survey of the native
 #'   pinewoods of Scotland 1971. Earth System Science Data, 8(1), 177-189.
@@ -26,25 +25,28 @@
 #'
 #' This is the environmental data from a series of plots across native Scots Pine
 #' woodlands in Scotland surveyed in 1971. It contains the approximate location of
-#' each site, the soil pH of each site plus the average annual rainfall, maximum summer
-#' temperature and minimum winter temperature from 1941-1970. All climate variables
-#' were calculated based upon the HadUK-Grid climate data (Met Office et al., 2022).
-#' For more information on the sites see Wood and Bunce (2016). All climate and soils
-#' data have been scaled such that they are centred approximately around zero and
-#' have a range within single digits.
+#' each site, the soil pH of each plot plus the average annual rainfall, maximum
+#' summer temperature and minimum winter temperature from 1941-1970. All climate
+#' variables were calculated based upon the HadUK-Grid climate data (Met Office et
+#' al., 2022). For more information on the sites see Wood and Bunce (2016).  This
+#' dataset has been limited to the 265 plots across 25 sites that have soil pH
+#' measurements.
 #'
 #' This data is made available under the terms of the Open Government Licence.
 #'
 #' @format
-#' A data frame with 25 rows and 7 columns.
+#' A data frame with 265 rows and 10 columns.
 #' \describe{
-#'   \item{Site_no}{The site identifier (27 in total)}
+#'   \item{Site_no}{The site identifier (25 in total)}
+#'   \item{Plot_no}{The plot identifier per site}
+#'   \item{SitePlot}{The unique plot identifier}
 #'   \item{OSGR}{The 1 km grid reference of each site (OSGB)}
-#'   \item{rainfall}{The average annual rainfall in mm, divided by 1000 and minus 1.5}
-#'   \item{tasmax_summer}{The average maximum temperature in summer in deg Celsius, minus 16}
-#'   \item{tasmin_winter}{The average minimum temperature in winter in deg Celsius, plus 1}
-#'   \item{Soil_PH}{The soil pH of the top 10cm of soil, minus 4.3}
-#'   \item{No_plots}{The difference between the number of plots surveyed and 16 (maximum number of plots surveyed)}
+#'   \item{Easting}{The Easting of each site (centre position of 1km square, British National Grid)}
+#'   \item{Northing}{The Northing of each site (centre position of 1km square, British National Grid)}
+#'   \item{rainfall}{The average annual rainfall in mm}
+#'   \item{tasmax_summer}{The average maximum temperature in summer in deg Celsius}
+#'   \item{tasmin_winter}{The average minimum temperature in winter in deg Celsius}
+#'   \item{Soil_PH}{The soil pH of the top 10cm of soil}
 #' }
 #'
 #' @references {
--- data-raw/pinewood.R
+++ data-raw/pinewood.R
@@ -6,7 +6,9 @@ library(dplyr)
 library(raster, exclude = "select")
 library(sf)
 
-site_locs <- read.csv("Scots_Pine_1971_Sites.csv")
+data.dir <- rstudioapi::askForSecret("Data directory")
+
+site_locs <- read.csv(paste0(data.dir,"Scots_Pine_1971_Sites.csv"))
 str(site_locs)
 
 # Convert to 1km and central point of 1 km
@@ -25,21 +27,21 @@ clim_files <- list.files(clim_folder)
 rainfall_files <- grep("rainfall", clim_files, value = TRUE)
 
 rain <- sapply(rainfall_files,function(x){
-  annual_rain <- brick(paste0(clim_folder,x))
+  annual_rain <- brick(paste0(clim_folder,"/",x))
   annual_siterain <- extract(annual_rain, site_locs_sf)
   annual_siterain
 })
 
 tasmax_files <- grep("tasmax", clim_files, value = TRUE)
 summer_tasmax <- sapply(tasmax_files,function(x){
-  tasmax <- brick(paste0(clim_folder,x))
+  tasmax <- brick(paste0(clim_folder,"/",x))
   annual_sitetasmax <- extract(tasmax, site_locs_sf)
   annual_sitetasmax[,3,drop=FALSE]
 })
 
 tasmin_files <- grep("tasmin", clim_files, value = TRUE)
 winter_tasmin <- sapply(tasmin_files,function(x){
-  tasmin <- brick(paste0(clim_folder,x))
+  tasmin <- brick(paste0(clim_folder,"/",x))
   annual_sitetasmin <- extract(tasmin, site_locs_sf)
   annual_sitetasmin[,1,drop=FALSE]
 })
@@ -51,7 +53,7 @@ site_locs2 <- mutate(site_locs2,
 str(site_locs2)
 
 # soil vars
-soils <- read.csv("PINEWOODS1971_SOIL_DATA.csv")
+soils <- read.csv(paste0(data.dir,"PINEWOODS1971_SOIL_DATA.csv"))
 str(soils)
 soils %>% mutate(LitterDepth = Litter_to - Litter_from,
                  OrganicDepth = Organic_to - Organic_from,
@@ -71,7 +73,7 @@ site_env <- select(site_locs2, Site_no = SITEID, OSGR, Easting, Northing,
   filter(SitePlot != "26P16" | !is.na(Soil_PH))
 
 # tree data
-trees <- read.csv("PINEWOODS1971_TREE_DATA.csv")
+trees <- read.csv(paste0(data.dir,"PINEWOODS1971_TREE_DATA.csv"))
 str(trees)
 tree_wide <- trees %>% mutate(SitePlot = paste0(Site_no, "P",Plot_no)) %>%
   filter(Dead == "") %>%
@@ -88,69 +90,93 @@ tree_wide <- trees %>% mutate(SitePlot = paste0(Site_no, "P",Plot_no)) %>%
                      values_from = n,
                      values_fill = 0)
 
-common_flora <- c("Calluna vulgaris", "Vaccinium myrtillus", "Potentilla erecta",
-                  "Deschampsia flexuosa", "Vaccinium vitis-idaea","Molinia caerulea",
-                  "Blechnum spicant", "Betula seedling/sp", "Agrostis canina sens.lat.",
-                  "Pteridium aquilinum", "Erica tetralix", "Carex echinata",
-                  "Sorbus aucuparia", "Galium saxatile","Narthecium ossifragum",
-                  "Luzula multiflora", "Erica cinerea", "Pinus sylvestris",
-                  "Viola riviniana/reichenbiana", "Melampyrum pratense",
-                  "Polygala serpyllifolia","Succisa pratensis", "Carex panicea",
-                  "Carex binervis", "Eriophorum vaginatum")
-
-flora <- read.csv("PINEWOODS1971_GROUND_FLORA.csv")
-str(flora)
-flora_wide <- flora %>% mutate(SitePlot = paste0(SITE_NO, "P",PLOT_NO)) %>%
-  filter(BRC_NAME %in% common_flora) %>%
-  rename(Species = BRC_NAME) %>%
-  count(SITE_NO, PLOT_NO, SitePlot, Species) %>%
-  mutate(Species = gsub(" ","_",Species)) %>%
-  mutate(Species = gsub("\\.", "", Species)) %>%
-  tidyr::pivot_wider(names_from = Species,
-                     values_from = n,
-                     values_fill = 0) %>%
-  rename(Viola_sp = `Viola_riviniana/reichenbiana`,
-         Pinus_sylvestris_seed = Pinus_sylvestris)
-
-combined_data <- full_join(tree_wide, flora_wide, by = c("Site_no" = "SITE_NO",
-                                                         "Plot_no" = "PLOT_NO",
-                                                         "SitePlot")) %>%
-  mutate(across(Pinus_sylvestris:Polygala_serpyllifolia, tidyr::replace_na, 0)) %>%
-  mutate(Betula_sp = Betula_sp + `Betula_seedling/sp`,
-         Pinus_sylvestris = Pinus_sylvestris + Pinus_sylvestris_seed) %>%
-  select(-Pinus_sylvestris_seed, -`Betula_seedling/sp`) %>%
-  arrange(SitePlot)
-str(combined_data)
-sum(combined_data$SitePlot %in% site_env$SitePlot)
-
-site_env <- filter(site_env, SitePlot %in% combined_data$SitePlot) %>%
+site_env_tree <- site_env %>%
+  select(Site_no:Soil_PH, SitePlot) %>%
+  filter(!is.na(Soil_PH) & SitePlot %in% tree_wide$SitePlot) %>%
+  select(Site_no, Plot_no, SitePlot, everything()) %>%
   arrange(SitePlot)
 
-# Complete jsdmstan format
-site_env <- filter(site_env, !is.na(Soil_PH)) %>%
-  select(Site_no, Plot_no, SitePlot, OSGR,
-         rainfall, tasmax_summer, tasmin_winter, Soil_PH)
-
-pinewood <- filter(combined_data, SitePlot %in% site_env$SitePlot) %>%
+tree_site <- tree_wide %>%
+  filter(SitePlot %in% site_env_tree$SitePlot) %>%
+  arrange(SitePlot) %>%
   tibble::column_to_rownames("SitePlot") %>%
-  select(-Site_no, -Plot_no)
-pinewood <- Y[site_env$SitePlot,]
-pinewood_env <- site_env
+  select(Pinus_sylvestris:Fraxinus_sp)
 
-pinewood <- pinewood %>% mutate(Site = pinewood_env$Site_no) %>% group_by(Site) %>%
-  summarise(across(everything(), function(x) sum(x > 0))) %>%
-  tibble::column_to_rownames("Site") %>%
-  select(-Prunus_padus, -Crataegus_monogyna, -Fraxinus_sp, -Populus_tremula,
-         -Pseudotsuga_menziesii)
-pinewood_env <- pinewood_env %>%
-  select(Site_no, OSGR, rainfall:Soil_PH) %>%
-  group_by(Site_no, OSGR) %>% summarise(across(.fns = mean),
-                                        No_plots = n(), .groups = "drop") %>%
-  mutate(rainfall = rainfall/1000 - 1.5,
-         tasmax_summer = tasmax_summer - 16,
-         tasmin_winter = tasmin_winter + 1,
-         Soil_PH = Soil_PH - 4.3,
-         No_plots = No_plots - 16)
+all.equal(site_env_tree$SitePlot, rownames(tree_site))
+
+pinewood <- tree_site
+pinewood_env <- site_env_tree
 
 usethis::use_data(pinewood, overwrite = TRUE)
 usethis::use_data(pinewood_env, overwrite = TRUE)
+
+# common_flora <- c("Calluna vulgaris", "Vaccinium myrtillus", "Potentilla erecta",
+#                   "Deschampsia flexuosa", "Vaccinium vitis-idaea","Molinia caerulea",
+#                   "Blechnum spicant", "Betula seedling/sp", "Agrostis canina sens.lat.",
+#                   "Pteridium aquilinum", "Erica tetralix", "Carex echinata",
+#                   "Sorbus aucuparia", "Galium saxatile","Narthecium ossifragum",
+#                   "Luzula multiflora", "Erica cinerea", "Pinus sylvestris",
+#                   "Viola riviniana/reichenbiana", "Melampyrum pratense",
+#                   "Polygala serpyllifolia","Succisa pratensis", "Carex panicea",
+#                   "Carex binervis", "Eriophorum vaginatum")
+#
+# flora <- read.csv(paste0(data.dir,"PINEWOODS1971_GROUND_FLORA.csv"))
+# str(flora)
+# flora_wide <- flora %>% mutate(SitePlot = paste0(SITE_NO, "P",PLOT_NO)) %>%
+#   filter(BRC_NAME %in% common_flora) %>%
+#   rename(Species = BRC_NAME) %>%
+#   count(SITE_NO, PLOT_NO, SitePlot, Species) %>%
+#   mutate(Species = gsub(" ","_",Species)) %>%
+#   mutate(Species = gsub("\\.", "", Species)) %>%
+#   tidyr::pivot_wider(names_from = Species,
+#                      values_from = n,
+#                      values_fill = 0) %>%
+#   rename(Viola_sp = `Viola_riviniana/reichenbiana`,
+#          Pinus_sylvestris_seed = Pinus_sylvestris)
+#
+# combined_data <- full_join(tree_wide, flora_wide, by = c("Site_no" = "SITE_NO",
+#                                                          "Plot_no" = "PLOT_NO",
+#                                                          "SitePlot")) %>%
+#   mutate(across(Pinus_sylvestris:Polygala_serpyllifolia, tidyr::replace_na, 0)) %>%
+#   mutate(Betula_sp = Betula_sp + `Betula_seedling/sp`,
+#          Pinus_sylvestris = Pinus_sylvestris + Pinus_sylvestris_seed) %>%
+#   select(-Pinus_sylvestris_seed, -`Betula_seedling/sp`) %>%
+#   arrange(SitePlot)
+# str(combined_data)
+# sum(combined_data$SitePlot %in% site_env$SitePlot)
+#
+# site_env <- filter(site_env, SitePlot %in% combined_data$SitePlot) %>%
+#   arrange(SitePlot)
+#
+# # Complete jsdmstan format
+# site_env <- site_env %>%
+#   # filter(!is.na(Soil_PH)) %>%
+#   select(Site_no, Plot_no, SitePlot, OSGR,
+#          rainfall, tasmax_summer, tasmin_winter, Soil_PH)
+#
+# pinewood <- filter(combined_data, SitePlot %in% site_env$SitePlot) %>%
+#   tibble::column_to_rownames("SitePlot") %>%
+#   select(-Site_no, -Plot_no)
+# pinewood <- pinewood[site_env$SitePlot,]
+# pinewood_env <- site_env
+#
+# pinewood <- pinewood %>% mutate(Site = pinewood_env$Site_no) %>% group_by(Site) %>%
+#   summarise(across(everything(), function(x) sum(x > 0))) %>%
+#   tibble::column_to_rownames("Site") %>%
+#   select(-Prunus_padus, -Crataegus_monogyna, -Fraxinus_sp, -Populus_tremula,
+#          -Pseudotsuga_menziesii)
+# pinewood_env <- pinewood_env %>%
+#   select(Site_no, OSGR, rainfall:Soil_PH) %>%
+#   group_by(Site_no, OSGR) %>% summarise(across(.fns = mean, na.rm = TRUE),
+#                                         No_plots = n(), .groups = "drop") %>%
+#   mutate(rainfall = rainfall/1000 - 1.5,
+#          tasmax_summer = tasmax_summer - 16,
+#          tasmin_winter = tasmin_winter + 1,
+#          Soil_PH = Soil_PH - 4.3,
+#          No_plots = No_plots - 16) %>%
+#   filter(No_plots == 0) %>%
+#   select(-No_plots)
+# pinewood <- pinewood[as.character(pinewood_env$Site_no),]
+#
+# usethis::use_data(pinewood, overwrite = TRUE)
+# usethis::use_data(pinewood_env, overwrite = TRUE)
